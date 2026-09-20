f408a9ab1e807099ada1d153efa28031e13c0409
Marcus <mvc7730@gmail.com>
2026-09-20 20:03:31 -0300

    commit inicial

---
+++ src/main/java/br/upe/catalogosdelivery/CatalogosdeliveryApplication.java
@@ -0,0 +1,13 @@
+package br.upe.catalogosdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogosdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogosdeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/upe/catalogosdelivery/CatalogosdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.upe.catalogosdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogosdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
